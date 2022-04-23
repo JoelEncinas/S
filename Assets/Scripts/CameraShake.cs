8160972476faaf95933f13f852ccd5cae149b9c6
JoelEncinas <joelencinasmartin@gmail.com>
2022-04-23 23:30:17 +0200

    added rotation to camera shake

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class CameraShake : MonoBehaviour
 {
     [SerializeField] private float shakeDuration = 1f;
-    [SerializeField] private float shakeMagnitude = 0.5f;
+    [SerializeField] private float shakeMagnitude = 0.1f;
 
     private Vector3 initialPosition;
+    private Quaternion initialRotation;
 
     void Start()
     {
         initialPosition = transform.position;
+        initialRotation = transform.rotation;
         Play();
     }
 
@@ -28,10 +30,12 @@ public class CameraShake : MonoBehaviour
         while(time <= shakeDuration)
         {
             transform.position = initialPosition + (Vector3)Random.insideUnitCircle * shakeMagnitude;
+            transform.rotation = Quaternion.Euler(0, 0, Random.Range(0f, 2f));
             time += Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
 
         transform.position = initialPosition;
+        transform.rotation = initialRotation;
     }
 }
